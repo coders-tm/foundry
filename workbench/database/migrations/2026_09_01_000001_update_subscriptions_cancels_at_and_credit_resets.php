843bc85b9a4b6e6818f371bbb7ba37772d052b0e
Dipak Sarkar <hello@dipaksarkar.in>
2026-09-01 15:08:45 +0530

    feat: rename canceled_at to cancels_at and add credit_resets_at to subscriptions table

---
+++ workbench/database/migrations/2026_09_01_000001_update_subscriptions_cancels_at_and_credit_resets.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            // Rename canceled_at to cancels_at
+            $table->renameColumn('canceled_at', 'cancels_at');
+
+            // Add credit_resets_at column
+            $table->timestamp('credit_resets_at')->nullable()->after('is_free_forever');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Do nothing
+    }
+};
